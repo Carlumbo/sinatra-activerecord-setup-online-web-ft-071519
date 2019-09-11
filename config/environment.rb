8dfa9ee0c1975d83bff649d4c389a8dc10e5fc19
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-11 20:29:47 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ configure :development do
   set :database, 
   'sqlite3:db/database.db'
   end 
-  #sets up connectiont to sqlite3 database named database.db and located in a folder called db, you could always hcange the anme of the file ehre to whwatver you may want 
+  #sets up connectiont to sqlite3 database named database.db and located in a folder called db, you could always hcange the anme of the file ehre to whwatver you may want. 
+  #rake will help create the files and/or folders 
 
 require './app
