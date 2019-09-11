d56e1ba2d95f2d0e56313a71b14f8725534021fa
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-11 20:29:23 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,6 @@ configure :development do
   set :database, 
   'sqlite3:db/database.db'
   end 
+  #sets up connectiont to sqlite3 database named database.db and located in a folder called db, you could always hcange the anme of the file ehre to whwatver you may want 
 
 require './app
